4c08fe3031e85b9c8cb57c75a0b82dc187c69d37
blakematis <14blake1994@gmail.com>
2019-01-28 15:53:40 -0800

    Removed all old test cases and migrating to JUnit.

--- src/test/java/OpenWeatherMap/CityTest.java
+++ src/test/java/OpenWeatherMap/CityTest.java
@@ -1,23 +1,5 @@
 package OpenWeatherMap;
 
-import org.junit.Test;
-
-import javax.json.JsonObject;
-import java.net.MalformedURLException;
-
-import static org.junit.Assert.assertTrue;
 
 public class CityTest {
-
-    @Test
-    public void testBuildCity(){
-        try {
-            JsonObject jsonObject = OpenWeatherMapUtil.jsonReply("https://samples.openweathermap.org/data/2.5/forecast?q=Roseville,us&appid=b6907d289e10d714a6e88b30761fae22");
-            OpenWeatherMapUtil.buildCity(jsonObject);
-            assertTrue(OpenWeatherMapUtil.buildCity(jsonObject) != null);
-        }catch (MalformedURLException e){
-            e.printStackTrace();
-        }
-
-    }
 }
--- src/test/java/OpenWeatherMap/CloudsTest.java
+++ src/test/java/OpenWeatherMap/CloudsTest.java
@@ -3,8 +3,5 @@ package OpenWeatherMap;
 import javax.json.JsonObject;
 
 public class CloudsTest{
-        public static void main(String[] args) throws Exception{
-            JsonObject jsonObject = OpenWeatherMapUtil.jsonReply("https://api.openweathermap.org/data/2.5/forecast?q=Roseville,US&units=imperial&appid=e513347207aae03c792f055e744790e4");
-            System.out.println(OpenWeatherMapUtil.buildClouds(jsonObject, 0));
-        }
+
 }
