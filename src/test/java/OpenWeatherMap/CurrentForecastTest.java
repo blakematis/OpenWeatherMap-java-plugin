2b3ea8e4b7d972f4cc1878812b6dd3e320bf8841
blakematis <14blake1994@gmail.com>
2019-01-28 15:41:36 -0800

    Basic buildJson method test to confirm forecast gets built and is not null.

---
+++ src/test/java/OpenWeatherMap/CurrentForecastTest.java
@@ -0,0 +1,30 @@
+package OpenWeatherMap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.json.JsonObject;
+import java.net.MalformedURLException;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class CurrentForecastTest {
+
+    private CurrentForecast currentForecast;
+
+    @Before
+    public void setupObject(){
+        try {
+            JsonObject jsonObject = OpenWeatherMapUtil.jsonReply("https://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b6907d289e10d714a6e88b30761fae22");
+            currentForecast = (CurrentForecast) currentForecast.build(jsonObject);
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testBuildMain(){
+        assertTrue(currentForecast != null);
+    }
+    
+}
