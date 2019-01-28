6865c791f591e023f839a1886be30da0ddead493
blakematis <14blake1994@gmail.com>
2019-01-28 15:51:35 -0800

    Removed old test case from ForecastTest needs to be updated to JUnit coverage.

--- src/test/java/OpenWeatherMap/ForecastTest.java
+++ src/test/java/OpenWeatherMap/ForecastTest.java
@@ -1,13 +1,6 @@
 package OpenWeatherMap;
 
-
-
-import javax.json.JsonObject;
-
 public class ForecastTest {
 
-    public static void main(String[] args) throws Exception{
-        JsonObject jsonObject = OpenWeatherMapUtil.jsonReply("https://api.openweathermap.org/data/2.5/forecast?q=Roseville,US&units=imperial&appid=e513347207aae03c792f055e744790e4");
-        System.out.println(OpenWeatherMapUtil.buildForecast(jsonObject, 0));
-    }
+
 }
